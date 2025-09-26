9018c62de2840ada080e76633649ecabef0c2f39
Rhazouani imadeddine <146865915+ImadRhaz@users.noreply.github.com>
2025-09-26 22:27:58 +0100

    Update produit-create.dto.ts

--- product-manager-frontend/src/app/dto/produit-create.dto.ts
+++ product-manager-frontend/src/app/dto/produit-create.dto.ts
@@ -1,16 +1,16 @@
 
 
 export interface ProduitCreateDTO {
-  // Propriétés obligatoires (sans '?' car elles sont dans les validations @NotBlank/@NotNull)
+  
   nom: string;
   prix: number;
   categorie: string;
 
-  // Propriétés optionnelles
+
   reference?: string | null;
   matricule?: string | null;
-  dateExpiration?: string | null; // Laisser en string pour la compatibilité avec le backend
+  dateExpiration?: string | null; 
 
-  // Le fichier uploadé
-  file: File | null; // Utilise le type File natif d'Angular/JS
+  
+  file: File | null; 
 }
