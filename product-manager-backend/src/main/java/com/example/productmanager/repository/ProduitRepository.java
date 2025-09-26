d0faa451af23eaae7ac74446db46fb5c86fd42bf
Rhazouani imadeddine <146865915+ImadRhaz@users.noreply.github.com>
2025-09-26 22:22:55 +0100

    Update ProduitRepository.java

--- product-manager-backend/src/main/java/com/example/productmanager/repository/ProduitRepository.java
+++ product-manager-backend/src/main/java/com/example/productmanager/repository/ProduitRepository.java
@@ -7,14 +7,9 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
 import java.util.List;
 
-@Repository // Indique que c'est un composant Spring de type Repository
+@Repository 
 public interface ProduitRepository extends JpaRepository<Produit, Long> {
 
-    /**
-     * Méthode personnalisée pour trouver les produits dont la date d'expiration est passée.
-     * Spring Data JPA dérive automatiquement l'implémentation de cette méthode.
-     * @param date La date limite pour vérifier l'expiration.
-     * @return Une liste de produits expirés.
-     */
+    
     List<Produit> findByDateExpirationBefore(LocalDate date);
 }
